18acbc4a1061b9c9a04a05e38a53e9ee2ec8d1ee
dragon-bn <sdelsuquet@sii.fr>
2020-09-15 07:46:37 +0200

    MAJ Dockerfile-runs.sh

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-docker container run -it monjenkins-v1
+
+#
+# https://medium.com/@jimkang/how-to-start-a-new-jenkins-container-and-update-jenkins-with-docker-cf628aa495e9
+#
+
+#docker container run -it monjenkins-v1
+
+# I'll keep it as "jenkins", you can use "jenkins_home" if you wish
+  if [ ! -d "${HOME}/jenkins" ];
+  then
+    mkdir --parents --verbose "${HOME}/jenkins";
+  fi
+
+
+
+
+# if you're using other names, replace $HOME/jenkins with your choice
+# in case you want to change port, use this
+# -e JENKINS_OPTS="--httpPort=80" (remember to change 8080:8080 below to 80:80)
+
+docker container run \
+--name jenkins \
+-p 8080:8080 -p 50000:50000 \
+-v "${HOME}/jenkins:/var/jenkins_home" \
+-d \
+monjenkins-v1
